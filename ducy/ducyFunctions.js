9d6d9e0f091af57ea616d1ad7be6c83b4af877a5
Thijs Boom <twboom@hotmail.nl>
2021-01-11 17:21:52 +0100

    Added ducyFunctions

---
+++ ducy/ducyFunctions.js
@@ -0,0 +1,14 @@
+function runDucy() {
+  let args = input.value.slice(5).trim().split(/ +/g);
+  let command = args.shift().toLowerCase();
+
+  switch(command) {
+    case 'install':
+      ducyInstall()
+      break
+
+    case 'help':
+      ducyHelp()
+      break
+  }
+}
